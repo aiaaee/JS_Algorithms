e7e737c875583be6b46c07ad75f20264aa6aec96
Amin Alaee <aminalaee4@gmail.com>
2024-07-29 10:32:20 +0430

    add new file

--- Prime_Numbers/script.js
+++ Prime_Numbers/script.js
@@ -1,1 +1,14 @@
-console.log("amin")
+let number = prompt("import your number : ");
+let is_prime = [] ; 
+
+if (number > 0 ){
+   for (let i = 0 ; i <= number ; i++){
+       if(number % i == 0 ){
+           is_prime.push(i)
+       }
+   }
+   is_prime.length > 2  ? alert("this is not prime") : alert("this is a prime ") 
+}
+
+for (const element of is_prime)
+   console.log(element)
