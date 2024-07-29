9c26f33bdf644c70875487d969580a6ad0826264
Amin Alaee <aminalaee4@gmail.com>
2024-07-29 11:09:13 +0430

    add new files

---
+++ Fibonacci/script.js
@@ -0,0 +1,25 @@
+// Computing Fibonacci 
+function fibonacci(n) {
+    if (n <= 1)
+        return n;
+    else
+        return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+console.log(fibonacci(12))
+
+console.log('-----------------')
+
+//fibonacci Series 
+let n = 5;
+let tmp;
+let num1 = 0;
+let num2 = 1;
+for (let i = 0; i < n; i++) {
+    tmp = num1 + num2
+    num1 = num2;
+    num2 = tmp;
+    tmp = num1;
+    console.log(tmp)
+
+}
---
+++ Prime_Numbers/script.js
@@ -0,0 +1,11 @@
+let number = prompt("import your number : ");
+let is_prime = [] ; 
+
+if (number > 0 ){
+   for (let i = 0 ; i <= number ; i++){
+       if(number % i == 0 ){
+           is_prime.push(i)
+       }
+   }
+   is_prime.length > 2  ? alert("this is not prime") : alert("this is a prime ") 
+}
