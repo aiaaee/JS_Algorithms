82b3429ab8564294a03a97cf3e2e12fec01d95a2
Amin Alaee <aminalaee4@gmail.com>
2024-07-30 23:12:14 +0430

    add new files

--- Remove_Duplicate/script.js
+++ Remove_Duplicate/script.js
@@ -1,1 +1,14 @@
-console.log("Hello")
+let arr1 = []
+for(let i = 0 ; i < 4 ; i++){
+    let a = prompt("Write your number: ")
+    arr1.push(a)
+}
+arr1 = arr1.map(item => parseInt(item))
+
+for(let j = 0 ; j < arr1.length ; j++){
+    for(let k = j+1 ; k < arr1.length ; k++ ){
+        if(arr1[j] == arr1[k]){
+            
+        }
+    }
+}
