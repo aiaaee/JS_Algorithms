e94d53e018aa20e9175bf0358dc150f1d5699a70
Amin Alaee <aminalaee4@gmail.com>
2024-08-03 13:30:19 +0430

    add

--- Merge_Two_Sorted_Array/script.js
+++ Merge_Two_Sorted_Array/script.js
@@ -1,21 +1,22 @@
-let array1 = []; 
-let array2 = [];
-function BubbleSort(array1 , array2){
+let array1 = []
+let array2 = []
+function bubleSort(array1 , array2){
     let array = array1.concat(array2)
-    let len = array.length; 
+    let len = array.length ;
     for(let i = 0 ; i < len ; i++){
         for(let j = 0 ; j < len - 1 ; j++){
             if(array[j] > array[j+1]){
-                let tmp = array[j] ;
-                array[j] = array[j+1] ;
-                array[j+1] = array[j] ;
+                let temp = array[j]
+                array[j] = array[j+1]
+                array[j+1] = temp
             }
         }
     }
     return array
 }
-let number1 = [1 , 11 , 22 , 32];
-let number2 = [1,  21 , 33 , 54];
 
-console.log(BubbleSort(number1 , number2))
+
+let numbers1 = [12 , 10 , 15 , 11 , 13];
+let numbers2 = [1, 22 , 42 , 4]
+console.log(bubleSort(numbers1 , numbers2))
 
