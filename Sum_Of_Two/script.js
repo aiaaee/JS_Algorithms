4d0241b7b67e8c67b2ae15e642a9cc173c668f26
Amin Alaee <112581116+aiaaee@users.noreply.github.com>
2024-08-09 00:14:57 +0330

    Update script.js

--- Sum_Of_Two/script.js
+++ Sum_Of_Two/script.js
@@ -1,1 +1,13 @@
 
+let arr = [1,2,3,4]
+const numb = 9
+for (let i = 0 ; i <= arr.length ; i++){
+    for(let j = 0 ; j <= arr.length ; j++){
+        if(arr[i] + arr[j] == numb ){
+            console.log("true")
+        }else{
+            console.log("false")
+        }
+    }
+        }
+
