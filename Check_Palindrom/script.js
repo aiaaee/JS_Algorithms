69e92565f3f03d8efc187ff48a2a8c88786b80ce
Amin Alaee <aminalaee4@gmail.com>
2024-08-08 14:07:50 +0430

    add new file

--- Check_Palindrom/script.js
+++ Check_Palindrom/script.js
@@ -1,1 +1,18 @@
 
+let str = 'JavaScript'.toLocaleLowerCase()
+let array = str.split('')
+let arr1 = []
+let arr2 = []
+for (let i = str.length - 1; i >= (str.length / 2); i--) {
+    arr1.push(array[i])
+}
+for (let i = 0; i < (str.length / 2); i++) {
+    arr2.push(array[i])
+}
+
+if (arr1.join('') == arr2.join('')) {
+    console.log("It's a palindrom")
+}
+else {
+    console.log("It's not a palindrom")
+}
