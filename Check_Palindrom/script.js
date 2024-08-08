5599e5fa1d2cadb164276ad78889f95759e9efe2
Amin Alaee <112581116+aiaaee@users.noreply.github.com>
2024-08-08 13:41:16 +0330

    Update script.js

--- Check_Palindrom/script.js
+++ Check_Palindrom/script.js
@@ -1,18 +1,22 @@
+function Palindrom(str){
+    str = str.toLowerCase()
+    let array = str.split('')
+    let arr1 = []
+    let arr2 = []
+    for (let i = str.length-1 ; i >= (str.length / 2) ; i--){
+        arr1.push(array[i])
+    }
+    for(let i = 0 ; i < (str.length /2 ) ; i++){
+        arr2.push(array[i])
+    }
 
-let str = 'JavaScript'.toLocaleLowerCase()
-let array = str.split('')
-let arr1 = []
-let arr2 = []
-for (let i = str.length - 1; i >= (str.length / 2); i--) {
-    arr1.push(array[i])
-}
-for (let i = 0; i < (str.length / 2); i++) {
-    arr2.push(array[i])
+    if(arr1.join('') == arr2.join('')){
+        console.log("It's a palindrom")
+    }
+    else{
+        console.log("It's not a palindrom")
+    }
 }
 
-if (arr1.join('') == arr2.join('')) {
-    console.log("It's a palindrom")
-}
-else {
-    console.log("It's not a palindrom")
-}
+
+Palindrom("Javascript")
