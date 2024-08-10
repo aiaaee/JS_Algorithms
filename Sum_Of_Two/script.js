16a99bef586bf1af85a8850a8899d6d28c863537
Amin Alaee <112581116+aiaaee@users.noreply.github.com>
2024-08-10 18:44:33 +0330

    Update script.js

--- Sum_Of_Two/script.js
+++ Sum_Of_Two/script.js
@@ -11,3 +11,22 @@ for (let i = 0 ; i <= arr.length ; i++){
     }
         }
 
+
+
+// function SumOfTwo(arr=[] , param) {
+//     let counter= 0
+//     for (let i = 0 ; i <= arr.length ; i++){
+//         for(let j = 0 ; j <= arr.length ; j++){ 
+//             arr[i] + arr[j] == param ? "Ok" : counter += 1
+//         }
+//     }
+//     if(counter > 0){
+//         console.log("It's true ")
+//     }
+//     else{
+//         console.log("It is not true")
+//     }
+// }
+        
+// //         
+// SumOfTwo([1 , 2 , 3] , )
