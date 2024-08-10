49dc2543b7f37b46aeb7b58157427ab7cdcfd6cf
Amin Alaee <112581116+aiaaee@users.noreply.github.com>
2024-08-10 18:52:18 +0330

    Update script.js

--- Sum_Of_Two/script.js
+++ Sum_Of_Two/script.js
@@ -1,32 +1,17 @@
-
-let arr = [1,2,3,4]
-const numb = 9
-for (let i = 0 ; i <= arr.length ; i++){
-    for(let j = 0 ; j <= arr.length ; j++){
-        if(arr[i] + arr[j] == numb ){
-            console.log("true")
-        }else{
-            console.log("false")
+function SumOfTwo(arr=[] , param) {
+    let counter= 0
+    for (let i = 0 ; i <= arr.length ; i++){
+        for(let j = 0 ; j <= arr.length ; j++){ 
+            arr[i] + arr[j] == param ? counter += 1 : ''
         }
     }
-        }
-
-
-
-// function SumOfTwo(arr=[] , param) {
-//     let counter= 0
-//     for (let i = 0 ; i <= arr.length ; i++){
-//         for(let j = 0 ; j <= arr.length ; j++){ 
-//             arr[i] + arr[j] == param ? "Ok" : counter += 1
-//         }
-//     }
-//     if(counter > 0){
-//         console.log("It's true ")
-//     }
-//     else{
-//         console.log("It is not true")
-//     }
-// }
+    console.log("The Sum of Count Number is " , counter)
+    if(counter > 0){
+        console.log("It's true ")
+    }
+    else{
+        console.log("It is not true")
+    }
+}
         
-// //         
-// SumOfTwo([1 , 2 , 3] , )
+SumOfTwo([1 , 2 , 3] , 14)
