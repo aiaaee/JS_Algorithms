62ac83aa85fa1928eeec6705db10a2fe9d29d7e0
Amin Alaee <aminalaee4@gmail.com>
2024-08-14 17:36:52 +0430

    add new file

--- Shapes/Traingle.js
+++ Shapes/Traingle.js
@@ -1,5 +1,5 @@
 function Traingle(n){
-    for (let i = 0 ; i < n ; i++){
+    for (let i = 0 ; i <= n ; i++){
         let strike = ' '.repeat(n)
         let stargs = '* '.repeat(i);
         console.log(strike + stargs)
@@ -8,4 +8,13 @@ function Traingle(n){
 
 Traingle(10)
 
+console.log('               ')
 
+function Traingle2(n){
+    for(let i = 0 ; i < n ; i++){
+        let strike = ' '.repeat(n);
+        let stargs = '* '.repeat(n-i) ;
+        console.log(strike + stargs);
+    }
+}
+Traingle2(10)
