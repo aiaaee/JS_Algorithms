feb99c90b9a161b56e61b3403956f3732971685a
Amin Alaee <112581116+aiaaee@users.noreply.github.com>
2024-08-17 23:29:31 +0330

    Update script.js

--- Vector Formula/script.js
+++ Vector Formula/script.js
@@ -1,1 +1,12 @@
+// Magnitude of a Vector formula 
 
+function VectorA(x , y , z ) {
+    let x = x ** 2 
+    let y = y ** 2 
+    let z = z ** 2
+    // ---------------------------------------------
+    let A = Math.sqrt((x ** 2) + (y ** 2 ) + (z ** 2))
+    return A 
+}
+
+VectorA(10 , 20 , 30 )
