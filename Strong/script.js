378c2d134d1c4528da30668592ec06f3796883c9
Amin Alaee <112581116+aiaaee@users.noreply.github.com>
2024-08-27 19:38:27 +0330

    Update script.js

--- Strong/script.js
+++ Strong/script.js
@@ -1,1 +1,17 @@
+// A strong number is such that the sum of it's factorial is the number itself
 
+let number = prompt('Enter Any Number to Check Strong Number : ') 
+let Main_Number = number 
+let sum = 0 
+while (number > 0 ){
+  let lastDigit = number % 10 
+  fact = 1 
+  for(let i = 0 ; i < lastDigit + 1 ; i++){
+    fact *= i 
+  }
+  sum += fact
+  number = number // 10 
+}
+if(number == Main_Number){
+  alert(Main_Number , 'is strong number ')
+}
