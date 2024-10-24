52f822d257f1cef19fe91fa9821090177e514d67
Amin Alaee <112581116+aiaaee@users.noreply.github.com>
2024-10-24 22:32:14 +0330

    Update script.js

--- Average/script.js
+++ Average/script.js
@@ -1,7 +1,6 @@
-// We're able to using arguments.length to compute lengths of Average parameter . 
-
 function Average(a , b , c = 0 ){
     let d = a + b + c ; 
     console.log(d / arguments.length)
 }
 Average(1 , 2 , 3 )
+
